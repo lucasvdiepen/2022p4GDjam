766c3d69c26bb294947ef8afae207b1a08afffb0
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2022-06-08 15:34:31 +0200

    Working on turret

--- GameJam/TileEvents/Turret.cs
+++ GameJam/TileEvents/Turret.cs
@@ -1,4 +1,5 @@
 ﻿using GameJam.Events;
+using GameJam.Game;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,9 @@ namespace GameJam.TileEvents
 {
     public class Turret : TimerBehaviour
     {
+        int xDirection = 0;
+        int yDirection = 1;
+
         public Turret(float shootDelay) : base(true, shootDelay)
         {
 
@@ -16,7 +20,12 @@ namespace GameJam.TileEvents
 
         public override void TimerTick(UpdateEvent updateEvent)
         {
-            
+            Shoot(updateEvent.GameContext, updateEvent.RenderObject);
+        }
+
+        private void Shoot(GameContext gameContext, RenderObject renderObject)
+        {
+            Vector2 spawnPosition = new Vector2(renderObject.rectangle.X + gameContext.tileSize * xDirection, renderObject.rectangle.Y + gameContext.tileSize * yDirection);
         }
     }
 }
