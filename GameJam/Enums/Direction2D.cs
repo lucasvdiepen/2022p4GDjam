0ecfd7ea18db70984436860cd1f93da9dc3eec66
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2022-06-08 10:49:33 +0200

    Removed references

--- GameJam/Enums/Direction2D.cs
+++ GameJam/Enums/Direction2D.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GameJam.Enums
+﻿namespace GameJam.Enums
 {
     public enum Direction2D
     {
