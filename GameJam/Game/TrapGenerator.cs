84aa548cddd7250386b8f7f8eab21692967bbc70
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2022-06-10 00:27:20 +0200

    Working on trap generator

--- GameJam/Game/TrapGenerator.cs
+++ GameJam/Game/TrapGenerator.cs
@@ -1,4 +1,5 @@
-﻿using GameJam.TileEvents;
+﻿using GameJam.Enums;
+using GameJam.TileEvents;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,11 @@ namespace GameJam.Game
 {
     public class TrapGenerator
     {
-        private ObjectBehaviour[] traps;
+        private readonly Dictionary<TrapType, Func<float, ObjectBehaviour>> traps = new Dictionary<TrapType, Func<float, ObjectBehaviour>>()
+        {
+            {TrapType.Trap, (time) => { return new Trap(time); } }
+        };
+
         private const int trapsPerRoom = 5;
 
         public void GenerateTraps(Room room)
