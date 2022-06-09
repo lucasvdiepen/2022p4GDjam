2a63989f028f167fc1d749b530c01f56923cf3f2
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2022-06-09 12:33:06 +0200

    Started working on trap generator

---
+++ GameJam/Game/TrapGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameJam.Game
+{
+    public class TrapGenerator
+    {
+        private const int traps = 5;
+
+        public void GenerateTraps(Room room)
+        {
+
+        }
+    }
+}
