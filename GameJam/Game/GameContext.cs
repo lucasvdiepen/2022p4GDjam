6ce8119218e507d47257e514a4a6751120db3a49
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2022-06-17 12:44:35 +0200

    Removed unused field

--- GameJam/Game/GameContext.cs
+++ GameJam/Game/GameContext.cs
@@ -9,14 +9,7 @@ namespace GameJam.Game
         internal int tileSize = 16;
         internal RenderObject player = new RenderObject();
         internal Health playerHealth = new Health(3);
-        internal RenderObject[] playerHealthObjects;
         internal SpriteMap spriteMap = new SpriteMap();
         internal Room room;
-
-        public GameContext()
-        {
-            playerHealthObjects = new RenderObject[playerHealth.StartHp];
-
-        }
     }
 }
